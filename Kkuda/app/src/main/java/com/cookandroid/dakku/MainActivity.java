6a7810e0a626747c3f3659f5caf9de3594306ee9
Yoon-Jiyeon <she79560@naver.com>
2022-12-05 23:37:53 +0900

    [BUG] duplicate classes 에러 남

--- Kkuda/app/src/main/java/com/cookandroid/dakku/MainActivity.java
+++ Kkuda/app/src/main/java/com/cookandroid/dakku/MainActivity.java
@@ -2,13 +2,87 @@ package com.cookandroid.dakku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
 public class MainActivity extends AppCompatActivity {
+    Context mContext;
+    FloatingActionButton fab_main, fab_sub1, fab_sub2;
+    Animation fab_open, fab_close;
+    boolean isFabOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = getApplicationContext();
+
+        fab_open = AnimationUtils.loadAnimation(mContext, R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(mContext, R.anim.fab_close);
+
+        fab_main = (FloatingActionButton) findViewById(R.id.fab_main);
+        fab_sub1 = (FloatingActionButton) findViewById(R.id.fab_sub1);
+        fab_sub2 = (FloatingActionButton) findViewById(R.id.fab_sub2);
+
+        fab_main.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                fabBtnClicked(view);
+            }
+        });
+        fab_sub1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                fabBtnClicked(view);
+            }
+        });
+        fab_sub2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                fabBtnClicked(view);
+            }
+        });
+    }
+
+    public void fabBtnClicked(View v) {
+        switch (v.getId()) {
+            case R.id.fab_main:
+                toggleFab();
+                break;
+            case R.id.fab_sub1:
+                toggleFab();
+                Toast.makeText(this, "Camera Open-!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.fab_sub2:
+                toggleFab();
+                Toast.makeText(this, "Map Open-!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    private void toggleFab() {
+        if (isFabOpen) {
+            fab_main.setImageResource(R.drawable.ic_plus);
+            fab_sub1.startAnimation(fab_close);
+            fab_sub2.startAnimation(fab_close);
+            fab_sub1.setClickable(false);
+            fab_sub2.setClickable(false);
+            isFabOpen = false;
+        } else {
+            fab_main.setImageResource(R.drawable.ic_down);
+            fab_sub1.startAnimation(fab_open);
+            fab_sub2.startAnimation(fab_open);
+            fab_sub1.setClickable(true);
+            fab_sub2.setClickable(true);
+            isFabOpen = true;
+        }
     }
 }
